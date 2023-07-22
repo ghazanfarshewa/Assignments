506d273a9fadafd1acb99ec2cf45db14db3dea0f
ghazanfarshewa <ghazanfarshewa29@gmail.com>
2023-07-22 16:22:40 +0500

    New folder created for Assignment #$0.8 in Assignment folder

---
+++ Assignments/Assignment #0.8/Assignment#0.8.py
@@ -0,0 +1,42 @@
+# 1) Perform studied python operators and save it in individual files inside assignment 
+# 2) Push it to your GitHub and send the GitHub link to me.
+
+x = 5
+y = 7
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(y%x)
+print(y//x)
+
+x+=4
+y-=2
+
+print(x)
+print(y)
+
+x*=5
+y/=x
+
+print(f"x is equal to {x}")
+print(f"y is equal to {y}")
+
+
+if y!=x:
+    print(y)
+
+if x>=123:
+    print("True")
+else:
+    print("False")
+
+if y and x <=100:
+    print("True")
+
+if not(x < 5 or x < 1):
+    print("True")
+
+
+
