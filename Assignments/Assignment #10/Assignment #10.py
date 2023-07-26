a9dfc49f8a4bcc393e1276cb3930ed4112103a1e
ghazanfarshewa <ghazanfarshewa29@gmail.com>
2023-07-26 12:23:04 +0500

    The new folder is created by name of Assignment#10 in folder of assignment and Assignment #10.py is added in it

---
+++ Assignments/Assignment #10/Assignment #10.py
@@ -0,0 +1,15 @@
+userage = int(input("Please enter your age"))
+
+if userage >= 25:
+    print("You are an adult")
+else:
+    print("You are a minor")
+
+for x in range(2,20):
+    if x %2 ==0:
+        print(x)
+
+i = 1
+while i <= 5:
+  print(i*i)
+  i += 1
